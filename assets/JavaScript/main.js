fabd5aa409368e59f59a046661c11744f1af86a3
matttali09 <matttaliancich09@gmail.com>
2018-11-12 14:36:00 -0500

    still broken logic because of undefined vars

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,78 +1,110 @@
-var chosenEnemy = false;
-var chosenChampion = false;
-var attack = 0;
-var health = 0;
-var counterAttack = 0;
+$(document).ready(function () {
+    //globals
+    var chosenEnemy = false;
+    var chosenChampion = false;
+    var attack = 0;
+    var health = 0;
+    var counterAttack = 0;
 
-var characters = {
-    getAttributes: function (char) {
-        if (chosenChampion === true) {
-            attack = char.attack + 10;
-            health = char.health;
-        }
-        else {
-            health = char.health;
-            counterAttack = char.getCounterAttack + 10;
-        }
-    },
-    levelUp: function (char1, char2) {
-        if (char2.health < 0) {
-            char.attack += 10;
-            char.health += 100;
-        }
-    },
-};
+    // run when chosing and attacking champions
+    var fightStats = {
+        getAttributes: function (char) {
+            if (chosenChampion === true) {
+                attack = char.attack + 10;
+                health = char.health;
+            }
+            else {
+                health = char.health;
+                counterAttack = char.getCounterAttack + 10;
+            }
+        },
+        levelUp: function (char1, char2) {
+            if (char2.health < 0) {
+                char.attack += 10;
+                char.health += 100;
+            }
+        },
+    };
 
-var characters = {
-    obiWan: {
-        health: 150,
-        attack: 20,
-        counterAttack: 15
-    },// best in game
-    lukeSkywalker: {
-        health: 100,
-        attack: 30,
-        counterAttack: 20
-    },
-    emporerPalpatine: {
-        health: 90,
-        attack: 25,
-        counterAttack: 20
-    }, // worst in game
-    darthVader: {
-        health: 100,
-        attack: 25,
-        counterAttack: 20
-    },
-};
+    // game characters and starting attributes
+    var characters = {
+        obiWan: {
+            health: 150,
+            attack: 20,
+            counterAttack: 15,
+            chosenChampion: false,
+            chosenEnemy: false,
+        },// best in game
+        lukeSkywalker: {
+            health: 100,
+            attack: 30,
+            counterAttack: 20,
+            chosenChampion: false,
+            chosenEnemy: false,
+        },
+        darthVader: {
+            health: 100,
+            attack: 25,
+            counterAttack: 20,
+            chosenChampion: false,
+            chosenEnemy: false,
+        },
+        emporerPalpatine: {
+            health: 90,
+            attack: 25,
+            counterAttack: 20,
+            chosenChampion: false,
+            chosenEnemy: false,
+        }, // worst in game
+    };
 
-var battle = function (char1, char2) {
-    char1.health -= char2.counterAttack
-    char2.health -= char1.attack
-};
+    // run on battle button
+    var battle = function (char1, char2) {
+        char1.health -= char2.counterAttack
+        char2.health -= char1.attack
+    };
 
-// character selection
-$("#char1").on("click", function () {
-    $("#char2").appendTo($("#enemies-sect"))
-    $("#char3").appendTo($("#enemies-sect"))
-    $("#char4").appendTo($("#enemies-sect"))
-    character[chosenChampion] = true;
-});
-$("#char2").on("click", function () {
-    $("#char1").appendTo($("#enemies-sect"))
-    $("#char3").appendTo($("#enemies-sect"))
-    $("#char4").appendTo($("#enemies-sect"))
-    character[chosenChampion] = true;
-});
-$("#char3").on("click", function () {
-    $("#char1").appendTo($("#enemies-sect"))
-    $("#char2").appendTo($("#enemies-sect"))
-    $("#char4").appendTo($("#enemies-sect"))
-    character[chosenChampion] = true;
-});
-$("#char4").on("click", function () {
-    $("#char1").appendTo($("#enemies-sect"))
-    $("#char2").appendTo($("#enemies-sect"))
-    $("#char4").appendTo($("#enemies-sect"))
-    character[chosenChampion] = true;
+    // $("#char1").attr(character.obiWan); not sure if i can use something like this
+    // for the data-type attribute i added to the img tag or if something like this would work better.
+
+
+    // character selection and movement between sections
+    if (chosenChampion === false ) {   
+    $("#char1").on("click", function () {
+        $("#char2").appendTo($("#enemies-sect1"))
+        $("#char3").appendTo($("#enemies-sect2"))
+        $("#char4").appendTo($("#enemies-sect3"))
+        $("#char1").character[this.chosenChampion] = true;
+    });
+    $("#char2").on("click", function () {
+        $("#char1").appendTo($("#enemies-sect1"))
+        $("#char3").appendTo($("#enemies-sect2"))
+        $("#char4").appendTo($("#enemies-sect3"))
+        $(this).character[this.chosenChampion] = true;
+    });
+    $("#char3").on("click", function () {
+        $("#char1").appendTo($("#enemies-sect1"))
+        $("#char2").appendTo($("#enemies-sect2"))
+        $("#char4").appendTo($("#enemies-sect3"))
+        $(this).character[this.chosenChampion] = true;
+    });
+    $("#char4").on("click", function () {
+        $("#char1").appendTo($("#enemies-sect1"))
+        $("#char2").appendTo($("#enemies-sect2"))
+        $("#char4").appendTo($("#enemies-sect3"))
+        $(this).character[this.chosenChampion] = true;
+    });}
+
+    if (chosenEnemy === false && chosenChampion === false) {
+    $("#ememies-sect1").on("click", function () {
+        $("#enemies-sect1").appendTo($("#defender-obj"))
+
+    });
+    $("#ememies-sect2").on("click", function () {
+        $("#enemies-sect2").appendTo($("#defender-obj"))
+    });
+    $("#ememies-sect3").on("click", function () {
+        $("#enemies-sect3").appendTo($("#defender-obj"))
+    });}
+
 });

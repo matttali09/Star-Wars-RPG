017bd052f2242b8648ec0836681a2cd5cfcedbdd
matttali09 <matttaliancich09@gmail.com>
2018-11-12 11:41:18 -0500

    wrapped my jQuery inside document.ready and put in the battle function need to append the characters to the images, and create the game function that runs all the rest

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -15,25 +15,40 @@ var characters = {
             counterAttack = char.getCounterAttack + 10;
         }
     },
-    levelUp: function (char) {
-        char.attack += 10;
-        char.health += 100;
-    }
+    levelUp: function (char1, char2) {
+        if (char2.health < 0) {
+            char.attack += 10;
+            char.health += 100;
+        }
+    },
 };
 
 var characters = {
-    obiWan: {health: 150,
+    obiWan: {
+        health: 150,
         attack: 20,
-        counterAttack: 15},// best in game
-    lukeSkywalker: {health: 100,
+        counterAttack: 15
+    },// best in game
+    lukeSkywalker: {
+        health: 100,
         attack: 30,
-        counterAttack: 20},
-    emporerPalpatine: {health: 90,
+        counterAttack: 20
+    },
+    emporerPalpatine: {
+        health: 90,
         attack: 25,
-        counterAttack: 20}, // worst in game
-    darthVader: {health: 100,
+        counterAttack: 20
+    }, // worst in game
+    darthVader: {
+        health: 100,
         attack: 25,
-        counterAttack: 20},
+        counterAttack: 20
+    },
+};
+
+var battle = function (char1, char2) {
+    char1.health -= char2.counterAttack
+    char2.health -= char1.attack
 };
 
 // character selection
@@ -42,22 +57,22 @@ $("#char1").on("click", function () {
     $("#char3").appendTo($("#enemies-sect"))
     $("#char4").appendTo($("#enemies-sect"))
     character[chosenChampion] = true;
-})
+});
 $("#char2").on("click", function () {
     $("#char1").appendTo($("#enemies-sect"))
     $("#char3").appendTo($("#enemies-sect"))
     $("#char4").appendTo($("#enemies-sect"))
     character[chosenChampion] = true;
-})
+});
 $("#char3").on("click", function () {
     $("#char1").appendTo($("#enemies-sect"))
     $("#char2").appendTo($("#enemies-sect"))
     $("#char4").appendTo($("#enemies-sect"))
     character[chosenChampion] = true;
-})
+});
 $("#char4").on("click", function () {
     $("#char1").appendTo($("#enemies-sect"))
     $("#char2").appendTo($("#enemies-sect"))
     $("#char4").appendTo($("#enemies-sect"))
     character[chosenChampion] = true;
-})
+});
